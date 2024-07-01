05dd90f3f5e3b862f712ad281da81aaa9855d428
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-07-01 18:25:00 +0200

    added login component

---
+++ app-front/src/app/Pages/login/login.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthenticationService } from '../../shared/services/authentification/authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  authService: AuthenticationService = inject(AuthenticationService);  // Injected to access the authentication service login method
+  router: Router = inject(Router);
+
+  emailAddress: string = '';
+  password: string = '';
+  displayErrorMessage: boolean = false;  // This is to display errors to the user
+
+  login () {
+    this.authService.login(this.emailAddress, this.password)
+    .then((res: boolean) => {
+      if (res) {
+        this.router.navigateByUrl('/tasks');
+      } else {
+        this.displayErrorMessage = true;
+      }
+    })
+  }
+}
